202f34bfe02be02deaeac914609fa44bae8c4ac7
joseng8908 <sengyun8908@naver.com>
2026-03-21 21:40:09 +0900

    feat: attach middleware in main.go

--- animal-face-go/cmd/server/main.go
+++ animal-face-go/cmd/server/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"animal-face-go/internal/dto"
 	"animal-face-go/internal/middleware"
 
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.New() // New를 사용해서 gin을 내 입맛대로 커스터마이징
+	// 커스텀마이징, 미들웨어 등록하기
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
 	r.Use(middleware.CORSMiddleware())
-
-	r.GET("/health")
+	r.Use(middleware.ErrorHandler())
 
 	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		response := dto.NewSuccessResponse(nil, "서버가 정상입니다")
+		c.JSON(http.StatusOK, response)
 	})
+
+	r.Run(":8080")
 }
